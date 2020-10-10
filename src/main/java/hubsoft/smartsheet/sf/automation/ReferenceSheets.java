da7eee8a190ec0508115b00ff4ed1cade7533499
Jilipop <jilipop@web.de>
2020-10-10 23:26:13 +0200

    Removed error handling - programm should fail if exception thrown here

--- src/main/java/hubsoft/smartsheet/sf/automation/ReferenceSheets.java
+++ src/main/java/hubsoft/smartsheet/sf/automation/ReferenceSheets.java
@@ -1,5 +1,6 @@
 package hubsoft.smartsheet.sf.automation;
 
+import com.smartsheet.api.SheetResources;
 import com.smartsheet.api.Smartsheet;
 import com.smartsheet.api.SmartsheetBuilder;
 import com.smartsheet.api.SmartsheetException;
@@ -14,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class ReferenceSheet implements CommandLineRunner{
+public class ReferenceSheets implements CommandLineRunner{
 
     private final Constants constants;
     private final Smartsheet smartsheet;
@@ -22,31 +23,27 @@ public class ReferenceSheet implements CommandLineRunner{
     private final static Map<Long, Sheet> sheets = new HashMap<>();
 
     @Autowired
-    public ReferenceSheet(Constants constants) {
+    public ReferenceSheets(Constants constants, Smartsheet smartsheet) {
         this.constants = constants;
-        smartsheet = new SmartsheetBuilder()
-                .setAccessToken(constants.getAccessToken())
-                .build();
+        this.smartsheet = smartsheet;
     }
 
     @Override
-    public void run(String... args){
+    public void run(String... args) throws SmartsheetException {
         for (long sheetId: constants.getInputSheetIds()) {
             Sheet sheet = save(sheetId);
             sheets.put(sheetId, sheet);
         }
     }
 
-    private Sheet save(long inputSheetId){
-        try {
-            Sheet sheet = smartsheet.sheetResources().getSheet(inputSheetId, null, EnumSet.of(ObjectExclusion.NONEXISTENT_CELLS), null, null, null, null, null);
-            System.out.println(sheet.getRows().size() + " Zeilen aus der Datei " + sheet.getName() + " geladen.");
+    private Sheet save(long inputSheetId) throws SmartsheetException {
+            SheetResources sheetResources = smartsheet.sheetResources();
+            Sheet sheet = sheetResources.getSheet(inputSheetId,
+                    null, EnumSet.of(ObjectExclusion.NONEXISTENT_CELLS),
+                    null, null, null,
+                    null, null);
+            System.out.println(sheet.getTotalRowCount() + " Zeilen aus der Datei " + sheet.getName() + " geladen.");
             return sheet;
-        } catch (SmartsheetException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Vorher-Zustand des Eingabe-Sheets konnte nicht gespeichert werden.");
-            return null;
-        }
     }
 
     public static Sheet getSheet(long sheetId) {
