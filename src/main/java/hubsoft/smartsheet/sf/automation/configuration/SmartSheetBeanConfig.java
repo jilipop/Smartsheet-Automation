809a21a18233f6f65cb13d9f58f5c0f11419857d
Jilipop <jilipop@web.de>
2020-10-10 23:26:47 +0200

    Turned this into a bean for easy injection

---
+++ src/main/java/hubsoft/smartsheet/sf/automation/configuration/SmartSheetBeanConfig.java
@@ -0,0 +1,26 @@
+package hubsoft.smartsheet.sf.automation.configuration;
+
+import com.smartsheet.api.Smartsheet;
+import com.smartsheet.api.SmartsheetBuilder;
+import hubsoft.smartsheet.sf.automation.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SmartSheetBeanConfig {
+
+    private final Constants constants;
+
+    @Autowired
+    public SmartSheetBeanConfig(Constants constants) {
+        this.constants = constants;
+    }
+
+    @Bean
+    public Smartsheet getSmartsheet() {
+        return new SmartsheetBuilder()
+                .setAccessToken(constants.getAccessToken())
+                .build();
+    }
+}
