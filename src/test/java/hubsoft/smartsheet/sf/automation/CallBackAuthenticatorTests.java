250f817c5dc0f9cdfc053813d04b1170a59c08ca
Jilipop <jilipop@web.de>
2020-10-15 15:53:19 +0200

    Stopped command line runner from starting during unit tests

--- src/test/java/hubsoft/smartsheet/sf/automation/CallBackAuthenticatorTests.java
+++ src/test/java/hubsoft/smartsheet/sf/automation/CallBackAuthenticatorTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.security.GeneralSecurityException;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class CallBackAuthenticatorTests {
 
     private final CallBackAuthenticator authenticator;
