ff2c5b9297c1bf979bd5ca06bda8da60097f1385
Jilipop <jilipop@web.de>
2020-10-17 00:09:12 +0200

    improved unit tests

--- src/test/java/hubsoft/smartsheet/sf/automation/ReferenceSheetTests.java
+++ src/test/java/hubsoft/smartsheet/sf/automation/ReferenceSheetTests.java
@@ -5,11 +5,14 @@ import com.smartsheet.api.models.Sheet;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Random;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,60 +22,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReferenceSheetTests {
 
-    private final Constants constants;
     private ReferenceSheets testReferenceSheets;
 
-    private static SmartsheetRepository mockRepository;
+    private final Set<Long> testSheetIds = Set.of(new Random().nextLong(), new Random().nextLong(), new Random().nextLong());
     private static final Sheet testSheet = new Sheet();
 
-    @Autowired
-    public ReferenceSheetTests(Constants constants) {
-        this.constants = constants;
-    }
+    @MockBean
+    private SmartsheetRepository mockRepository;
+
+    @MockBean
+    private Constants mockConstants;
+
 
     @BeforeAll
     public static void setup() {
-        mockRepository = Mockito.mock(SmartsheetRepository.class);
-        testSheet.setId(123456L);
+        testSheet.setId(new Random().nextLong());
         testSheet.setName("empty test sheet");
     }
 
     @BeforeEach
     public void resetInstance() {
-        testReferenceSheets = new ReferenceSheets(constants, mockRepository);
+        testReferenceSheets = new ReferenceSheets(mockConstants, mockRepository);
     }
 
     @Test
     @Order(1)
-    @DisplayName("before run(), the sheets map does not contain any entries matching the sheet ids in constants")
+    @DisplayName("before run(), the sheets map does not contain any entries matching the sheet ids")
     public void testSheetMapContentsBeforeRun() {
-        for (long sheetId: constants.getInputSheetIds()){
-            assertThat(ReferenceSheets.getSheet(sheetId)).isNull();
-        }
+       testSheetIds.forEach(sheetId -> assertThat(ReferenceSheets.getSheet(sheetId)).isNull());
     }
 
     @Test
     @Order(2)
-    @DisplayName("during run() all sheet ids in constants are requested from the repository")
+    @DisplayName("during run() all sheet ids are requested from the repository")
     public void testRepositorySheetRequests() throws SmartsheetException {
+        Mockito.when(mockConstants.getInputSheetIds()).thenReturn(testSheetIds);
+
         testReferenceSheets.run();
 
-        for (long sheetId: constants.getInputSheetIds()) {
+        for (long sheetId: testSheetIds) {
             Mockito.verify(mockRepository).getInputSheet(sheetId);
         }
     }
 
     @Test
     @Order(3)
-    @DisplayName("after run(), the sheets map contains entries for all sheet ids in constants")
+    @DisplayName("after run(), the sheets map contains entries for all sheet ids")
     public void testSheetMapContentsAfterRun() throws SmartsheetException {
-        for (long sheetId : constants.getInputSheetIds()) {
+        Mockito.when(mockConstants.getInputSheetIds()).thenReturn(testSheetIds);
+        for (long sheetId : testSheetIds) {
             Mockito.when(mockRepository.getInputSheet(sheetId)).thenReturn(new Sheet(sheetId));
         }
 
         testReferenceSheets.run();
 
-        for (long sheetId : constants.getInputSheetIds()) {
+        for (long sheetId : testSheetIds) {
             assertThat(ReferenceSheets.getSheet(sheetId).getId()).isEqualTo(sheetId);
         }
     }
