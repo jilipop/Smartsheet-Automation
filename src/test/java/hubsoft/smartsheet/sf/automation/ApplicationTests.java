82f451c911eca07107caeddef4d02bea9ff1b71d
Jilipop <jilipop@web.de>
2020-10-16 17:39:20 +0200

    Stopped command line runner from starting during unit tests

--- src/test/java/hubsoft/smartsheet/sf/automation/ApplicationTests.java
+++ src/test/java/hubsoft/smartsheet/sf/automation/ApplicationTests.java
@@ -2,8 +2,10 @@ package hubsoft.smartsheet.sf.automation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
