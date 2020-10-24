f06aa137e7615cd5435da4d85956e5d83a1bd4bc
Jilipop <jilipop@web.de>
2020-10-24 23:39:41 +0200

    moved tests out of spring context to make them true unit tests

--- src/test/java/hubsoft/smartsheet/sf/automation/WebHookControllerTests.java
+++ src/test/java/hubsoft/smartsheet/sf/automation/WebHookControllerTests.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hubsoft.smartsheet.sf.automation.models.Callback;
 import hubsoft.smartsheet.sf.automation.models.Event;
+import hubsoft.smartsheet.sf.automation.models.HookResponseBody;
 import hubsoft.smartsheet.sf.automation.models.VerificationRequest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,6 +36,7 @@ public class WebHookControllerTests {
     private static Callback eventCallBack;
     private static Callback statusChangeCallBack;
     private static VerificationRequest verificationRequest;
+    private static HookResponseBody hookResponseBody;
 
     private MockMvc mockMvc;
 
@@ -42,11 +46,14 @@ public class WebHookControllerTests {
     @Mock
     private CallBackAuthenticator mockAuthenticator;
 
+    @Mock
+    private ObjectMapper mockMapper;
+
     @InjectMocks
     private WebHookController controller;
 
     @BeforeAll
-    public static void setupObjects(){
+    public static void setupTestData(){
         verificationRequest = new VerificationRequest(
                 "d78dd1d3-01ce-4481-81de-92b4f3aa5ab1",
                 2674017481058180L
@@ -61,11 +68,11 @@ public class WebHookControllerTests {
                 4509506114742148L,
                 "DISABLED_SCOPE_INACCESSIBLE"
         );
+        hookResponseBody = new HookResponseBody(verificationRequest.getChallenge());
     }
 
     @BeforeEach
-    public void setupSutAndMockMvc() {
-        WebHookController controller = new WebHookController(mockService, mockAuthenticator, new ObjectMapper());
+    public void setupMockMvc() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .build();
     }
@@ -73,6 +80,7 @@ public class WebHookControllerTests {
     @Test
     @DisplayName("When event callback gets authenticated, process templates and return 200")
     public void testEventCallbackSuccess() throws Exception {
+        Mockito.when(mockMapper.readValue(anyString(), eq(Callback.class))).thenReturn(eventCallBack);
         Mockito.when(mockAuthenticator.authenticate(any(), any(), anyLong())).thenReturn(true);
 
         mockMvc.perform(post("/smartsheet")
@@ -87,10 +95,12 @@ public class WebHookControllerTests {
 
     @Test
     @DisplayName("When JSON processing of callback fails, throw JsonProcessingException")
-    public void testCallBackJSONError () {
+    public void testCallBackJSONError () throws JsonProcessingException {
+        Mockito.when(mockMapper.readValue(anyString(), eq(Callback.class))).thenThrow(JsonProcessingException.class);
+
         assertThatThrownBy(() -> mockMvc.perform(post("/smartsheet")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("Non-JSON-String")
+                .content(asJsonString(eventCallBack))
                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn()
         ).isInstanceOf(JsonProcessingException.class);
@@ -100,6 +110,7 @@ public class WebHookControllerTests {
     @Test
     @DisplayName("When status change callback gets authenticated, return 200")
     public void testStatusChangeCallback() throws Exception {
+        Mockito.when(mockMapper.readValue(anyString(), eq(Callback.class))).thenReturn(statusChangeCallBack);
         Mockito.when(mockAuthenticator.authenticate(any(), any(), anyLong())).thenReturn(true);
 
         mockMvc.perform(post("/smartsheet")
@@ -112,6 +123,7 @@ public class WebHookControllerTests {
     @Test
     @DisplayName("When authentication fails, return 403")
     public void testAuthenticationFailure() throws Exception {
+        Mockito.when(mockMapper.readValue(anyString(), eq(Callback.class))).thenReturn(eventCallBack);
         Mockito.when(mockAuthenticator.authenticate(any(), any(), anyLong())).thenReturn(false);
 
         mockMvc.perform(post("/smartsheet")
@@ -124,7 +136,11 @@ public class WebHookControllerTests {
     @Test
     @DisplayName("When a healthy verification request arrives, answer it correctly")
     public void testVerificationResponseWithHealthyBody() throws Exception {
+        Mockito.when(mockMapper.readValue(anyString(), eq(VerificationRequest.class))).thenReturn(verificationRequest);
+        Mockito.when(mockMapper.writeValueAsString(notNull())).thenReturn(asJsonString(hookResponseBody));
+        ArgumentCaptor<HookResponseBody> hookResponseBodyCaptor = ArgumentCaptor.forClass(HookResponseBody.class);
         String challengeString = verificationRequest.getChallenge();
+
         mockMvc.perform(post("/smartsheet")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(verificationRequest))
@@ -133,12 +149,17 @@ public class WebHookControllerTests {
                 .andExpect(header().string("Smartsheet-Hook-Response", challengeString))
                 .andExpect(jsonPath("$.smartsheetHookResponse").value(challengeString))
                 .andExpect(status().isOk());
+
+        Mockito.verify(mockMapper).writeValueAsString(hookResponseBodyCaptor.capture());
+        assertThat(hookResponseBodyCaptor.getValue()).isEqualToComparingFieldByField(hookResponseBody);
     }
 
     @Test
     @DisplayName("When a verification request arrives but the body can't be read, send back the challenge header and status 200")
     public void testVerificationResponseWhenBodyIsUnreadable() throws Exception {
+        Mockito.when(mockMapper.readValue(anyString(), eq(VerificationRequest.class))).thenThrow(JsonProcessingException.class);
         String challengeString = verificationRequest.getChallenge();
+
         mockMvc.perform(post("/smartsheet")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("Non-JSON-String")
