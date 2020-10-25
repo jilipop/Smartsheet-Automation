9ad36f2530347e13340b93dfee92c8de0cc06f88
Jilipop <jilipop@web.de>
2020-10-25 15:45:32 +0100

    removed spring container load test

--- src/test/java/hubsoft/smartsheet/sf/automation/ApplicationTests.java
+++ src/test/java/hubsoft/smartsheet/sf/automation/ApplicationTests.java
@@ -1,15 +1,8 @@
 package hubsoft.smartsheet.sf.automation;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
